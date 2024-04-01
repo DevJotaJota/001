cd2d152a77209a041b18fba89e782de68c6c4b5a
João José Chagas Neto <joaojosechagasneto2005@gmail.com>
2024-04-01 14:55:44 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,24 @@
+alert('Boas vindas ao nosso site.');
+
+//declaração de variáveis(let) e para verificar se as variáveis estão funcionando(console.log)
+let nome = prompt('Qual é o seu nome?');
+console.log(nome);
+let idade = prompt ('Digite a sua idade.');
+console.log(idade);
+let numeroDeVendas = 50;
+console.log(numeroDeVendas);
+let saldoDisponivel = 1000;
+console.log(saldoDisponivel);
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+console.log(mensagemDeErro);
+
+//condição verdadeira
+if(idade >= 18)
+    {
+        alert('Pode tirar a habilitação!');
+    }
+
+//exibir mensagem de erro
+alert(mensagemDeErro);
+
+
